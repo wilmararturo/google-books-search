20f70218e68465afdf00a04e4ee3f7420dac5bfe
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2021-01-08 01:10:24 -0500

    updated mongo connection string

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://user1:password1@ds125871.mlab.com:25871/heroku_0xn0jnk7",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/google-books-search",
   {
     useCreateIndex: true,
-    useNewUrlParser: true
+    useNewUrlParser: true,
   }
 );
 
