cf1b22bbe2c629e339df4e0f87fca46b71d37f79
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2021-01-10 23:45:59 -0500

    lint fixes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const compression = require("compression");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -13,6 +13,8 @@ app.use(logger("dev"));
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(compression());
+
 // Serve up static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -31,6 +33,6 @@ mongoose.connect(
 );
 
 // Start the API server
-app.listen(PORT, () =>
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
-);
+app.listen(PORT, () => {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
