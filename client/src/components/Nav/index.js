91036fbde74f1c50dde310b0257fb45fba2200aa
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2021-01-16 23:31:22 -0500

    styling and ES6 refactor

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,77 +1,80 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 
-class Nav extends Component {
-  state = {
-    open: false,
-    width: window.innerWidth
-  };
+function Nav() {
+  const [open, setOpen] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
 
-  updateWidth = () => {
-    const newState = { width: window.innerWidth };
+  useEffect(() => {
+    window.addEventListener("resize", updateWidth());
+    return () => {
+      window.removeEventListener("resize", updateWidth());
+    };
+  }, []);
 
-    if (this.state.open && newState.width > 991) {
-      newState.open = false;
+  const updateWidth = () => {
+    if (open && width > 991) {
+      setOpen(false);
     }
-
-    this.setState(newState);
+    setWidth(window.innerWidth);
   };
 
-  toggleNav = () => {
-    this.setState({ open: !this.state.open });
+  const toggleNav = () => {
+    setOpen(!open);
   };
 
-  componentDidMount() {
-    window.addEventListener("resize", this.updateWidth);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.updateWidth);
-  }
-
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light mb-2">
-        <Link className="navbar-brand" to="/">
-          Google Books
-        </Link>
-        <button
-          onClick={this.toggleNav}
-          className="navbar-toggler"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className={`${this.state.open ? "" : "collapse "}navbar-collapse`} id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link
-                onClick={this.toggleNav}
-                className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}
-                to="/"
-              >
-                Search
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                onClick={this.toggleNav}
-                className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
-                to="/saved"
-              >
-                Saved
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark mb-2">
+      <Link className="navbar-brand" to="/">
+        My Google Books
+      </Link>
+      <button
+        onClick={toggleNav}
+        className="navbar-toggler"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div
+        className={`${open ? "" : "collapse "}navbar-collapse`}
+        id="navbarNav"
+      >
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              onClick={toggleNav}
+              className={
+                window.location.pathname === "/"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+              to="/"
+            >
+              Search
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              onClick={toggleNav}
+              className={
+                window.location.pathname === "/saved"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+              to="/saved"
+            >
+              Saved
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Nav;
